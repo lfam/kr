21eb3ada35c75edd31d14164e8cae01ae74b1017
Leo Famulari <lfamular@gmail.com>
2015-01-08 15:24:03 -0500

    1.23 pseudo-code

---
+++ 1.23.c
@@ -0,0 +1,110 @@
+/*
+ * This is a program to remove comments from C programs. It will handle both
+ * c89 and c99 style comments.
+ *
+ * c99 style (slash slash):
+ * if (slash slash), getchar() until newline, putchar() newline.
+ * if (c == '\n') break;
+ *
+ * c89 style (slash asterisk):
+ * if slash, then if asterisk
+ * getchar() until asterisk, then if slash, else repeat
+ * this is the first place where we call getchar() inside the main loop
+ * 
+ * It must handle quoted strings and literals correctly. This means not
+ * looking for comments between "s and 's.
+ *
+ * Finally, it should handle escaped characters. This means \. If we encounter
+ * a \, and we are not in a comment, pass it and next character. This may be
+ * the second place where we want to call getchar() within the main getchar()
+ * loop.
+ *
+ * The question is, how do we put these requirements together in order to make
+ * the program work correctly?
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define IN 1
+#define OUT 0
+
+void die(int);
+void
+die(int status)
+{
+	exit(status);
+}
+
+int
+main()
+{
+	/*
+	 * The four special states we can be in. 
+	 *
+	 * Luckily, these states are exclusive. Meaning, if we are in a quote,
+	 * we cannot be in a constant or comment, and the same for the other
+	 * three states.
+	 */
+	int quote = 0;
+	int constant = 0;
+	int comment89 = 0;
+	int comment99 = 0;
+
+	int c;
+	while ((c = getchar()) != EOF) {
+/*
+		if quote
+			echo
+			if c == \"
+				quote = OUT
+		continue
+
+		if constant
+			echo
+			if c == \'
+				constant = OUT
+		continue		
+
+		if comment89
+			if c == *
+				getchar
+				if c == /
+					comment89 = OUT
+		continue
+
+		if comment99
+			if c == \n
+				comment99 = OUT
+		continue
+
+	Done with state checking, into normal mode 
+		if c == \\
+			putchar
+			getchar
+			putchar
+			continue
+		if c == /
+			buf = /
+			getchar
+			if c == *
+				comment89 = IN
+			else if c == /
+				comment99 = IN
+			else
+				put buf
+				putchar
+			continue
+		if c == \"
+			quote = IN
+			putchar
+			continue
+		if c == '
+			constant = IN
+			putchar
+			continue
+*/
+	}
+
+	return 0;
+}
