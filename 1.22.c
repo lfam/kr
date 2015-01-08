91f522e0a6ae4977227e182dcce68c6ea7b11261
Leo Famulari <lfamular@gmail.com>
2015-01-08 02:45:21 -0500

    1.22 done

--- 1.22.c
+++ 1.22.c
@@ -20,20 +20,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-int break_word(char *, int, int);
+int butcher(char *, int, int);
 
 int
-break_word(char *word, int w, int max)
+butcher(char *word, int w, int max)
 {
 	int j;
-	for (j = 0; j <= w; j++) {
+	int i = 0;
+	for (j = 0; j < w; j++) {
 		if ((! (j % max)) && (j != 0)) {
 			putchar(10);
+			i = 0;
 		}
 		putchar(word[j]);
-		fflush(stdout);
+		i++;
 	}
-	return(w % max);
+	return(max - i);
 }
 
 int
@@ -42,41 +44,40 @@ main()
 	int c;
 	char word[1024] = {0};
 	int w = 0;
-	int max = 6;
+	int max = 16;
 	int rem = max;
-
-	fprintf(stdout, "012345\n");
 
 	while ((c = getchar()) != EOF) {
 		word[w] = c;
-		w++;
-
-		fprintf(stderr, "\n-->%d\t", w);
-		fprintf(stderr, "%d-->", rem);
+		c != '\n' && w++;
 
 		if (c != ' ' && c != '\n' && c != '\t') {
 			continue;
 		}
 		if (w > max) {
-			fprintf(stderr, "MAX\n");
-			rem = break_word(word, w, max);
-			w = 0;
-			memset(word, 0, sizeof(word));
-			continue;
+			rem = butcher(word, w, max);
+			while (--w > 0) {
+				word[w] = '\0';
+			}
+		} else {
+			if (w > rem) {
+				putchar('\n');
+				rem = max;
+			}
+			fputs(word, stdout);
+			if (c == '\n') {
+				rem = max;
+			} else {
+				rem = rem - w;
+			}
+			while (--w > 0) {
+				word[w] = '\0';
+			}
 		}
-
-		if (w > rem) {
-			fprintf(stderr, "NO REM");
-			putchar('\n');
-			fflush(stdout);
-			rem = max;
-		}
+	}
+	/* this conditional handles inputs that don't end in \n */
+	if (w != 0) {
 		fputs(word, stdout);
-		fflush(stdout);
-		c != '\n' ? (rem = rem - w) : (rem = max);
-		while (--w > 0) {
-			word[w] = '\0';
-		}
 	}
 	return(0);
 }
