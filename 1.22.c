204e784b44bdfe009164efb7da1a9d34e3f841fe
Leo Famulari <lfamular@gmail.com>
2015-01-23 22:48:54 -0500

    1.22 fixed segfault on repeated newlines

--- 1.22.c
+++ 1.22.c
@@ -10,7 +10,7 @@
  * if word length is greater than remaining length, print newline
  * print word
  *
- * read next char into buffer until ' ', \n, \t, EOF, or \0 and record length
+ * OBSOLETE: read next char into buffer until ' ', \n, \t, EOF, or \0 and record length
  * decrement from max line length for each character read
  * if remaining is less than word, insert newline, else print word
  * if word length is greater than max, split word every max characters
@@ -21,20 +21,32 @@
 #include <string.h>
 
 int butcher(char *, int, int);
-
+/*
+ * This is for words that are longer than the max line length. We fill out the
+ * remaining space on the line, and then print the rest of the word, inserting
+ * a newline if we are at line max.
+ *
+ * Takes a char *, its length, the remaining space on current line, and the max line
+ * length. returns remaining space on line (ie, int rem);
+ *
+ * count down rem to 0, then print newline and rem = max
+ */
 int
-butcher(char *word, int len, int max)
+butcher(char *word, int rem, int max)
 {
-	int i, j;
-	i = j = 0;
-	for (; j < len; i++, j++) {
-		if ((! (j % max)) && (j != 0)) {
+	int j = 0;
+	while (word[j] != '\0') {
+		/* don't start a line with ' ' */
+		if ((rem == max) && (word[j] == ' ')) {
+			++j;
+		} else if (rem-- > 0) {
+			putchar(word[j++]);
+		} else {
 			putchar('\n');
-			i = 0;
+			rem = max;
 		}
-		putchar(word[j]);
 	}
-	return(max - i);
+	return(rem + 1);
 }
 
 int
@@ -42,22 +54,25 @@ main()
 {
 	int c;
 	char word[1024] = {0};
-	int w = 0;
+	unsigned int w = 0;
 	int max = 5;
 	int rem = max;
 
 	while ((c = getchar()) != EOF) {
-		if ((word[w] = c) != '\n') w++;
+		if ((word[w] = c) != '\n') {
+			w++;
+		}
 
 		if (c != ' ' && c != '\n' && c != '\t') {
 			continue;
 		}
 		if (w > max) {
 			rem = butcher(word, w, max);
-			while (--w > 0) {
-				word[w] = '\0';
+			while (w > 0) {
+				word[--w] = '\0';
 			}
-		} else {
+		} 
+		else {
 			if (w > rem) {
 				putchar('\n');
 				rem = max;
@@ -68,8 +83,8 @@ main()
 			} else {
 				rem = rem - w;
 			}
-			while (--w > 0) {
-				word[w] = '\0';
+			while (w > 0) {
+				word[--w] = '\0';
 			}
 		}
 	}
