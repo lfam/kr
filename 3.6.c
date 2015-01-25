4230fb45db758100baed1c73cad81acceee9a7bf
Leo Famulari <lfamular@gmail.com>
2015-01-25 17:33:17 -0500

    3.6 WIP

---
+++ 3.6.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include "../lib/binary_print.h"
+void itoa(int, char [], unsigned int);
+void reverse(char [], int);
+
+/* itoa:  convert n to characters in s, left padding to width w */
+void
+itoa(int n, char s[], unsigned int w)
+{
+	int i, sign;
+
+	if ((sign = n) < 0)  /* record sign */
+		n = -n;          /* make n positive */
+	i = 0;
+	do {      /* generate digits in reverse order */
+		s[i++] = abs(n % 10) + '0';  /* get next digit */
+		fprintf(stderr, "%c\n", s[i - 1]);
+	} while (n /= 10);    /* delete it */
+	if (sign < 0)
+		s[i++] = '-';
+	s[i] = '\0';
+	reverse(s, i);
+}
+	
+void
+reverse(char line[], int len)
+{
+	int i = 0;
+	int j = len - 1;
+
+	while (i <= len - 1) {
+		putchar(line[j]);
+		i++;
+		j--;
+	}
+	putchar('\n');
+}
+
+int
+main(void)
+{
+	char line[1024];
+	fprintf(stderr, PRINTF_BYTE32"\n", BYTE32(127));
+	fprintf(stderr, PRINTF_BYTE32"\n", BYTE32(-128));
+	fprintf(stderr, PRINTF_BYTE32"\n", BYTE32(INT_MIN));
+	itoa(INT_MIN, line, 10);
+	fprintf(stderr, PRINTF_BYTE32"\n", BYTE32((INT_MIN + 1)));
+	return 0;
+}
+
