0c613d39834a666558d3b7b871270d7af51978c5
Leo Famulari <lfamular@gmail.com>
2015-02-05 13:18:54 -0500

    remove old prompt

--- 4.9.c
+++ 4.9.c
@@ -1,7 +1,3 @@
-/* Exercise 4-6. Add commands for handling variables. (It's easy to 
- * provide twenty-six variables with single-letter names.) Add a variable 
- * for the most recently printed value. 
- */
 #include <stdio.h>
 #include <stdlib.h> /* for atof() */
 #include <ctype.h> /* for isdigit() */
