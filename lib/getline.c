5d723b3207056c0058efeaaf31c92ec7f3487c29
Leo Famulari <lfamular@gmail.com>
2015-02-15 14:23:04 -0500

    getline: use pointers internally

--- lib/getline.c
+++ lib/getline.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int
-getline(char s[], int max) {
+getline(char *s, int max) {
 	int i, c;
-	for (i = 0; i < max - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
-		s[i] = c;
+	char *p = s;
+	for (i = 0; i < max - 1 && (c = getchar()) != EOF && c != '\n' && c != 0; ++i) {
+		*s++ = c;
 	}
 	if (c == '\n') {
-		s[i++] = c;
+		*s++ = '\n';
 	}
-	s[i] = '\0';
-	return i;
+	*s = '\0';
+	return s - p;
 }
